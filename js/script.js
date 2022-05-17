6e7ed26a630804235dcdd4730a2d3b8e90b77b1d
Charmaine Lee <charmainelhm@gmail.com>
2022-05-17 20:45:06 +0800

    Finalise layout

--- js/script.js
+++ js/script.js
@@ -4,10 +4,23 @@ const closeNavBtn = document.querySelector(".nav__button-close");
 const navContent = document.querySelector(".nav-content");
 const navBgPanel = document.querySelector(".background-panel");
 const navBtnGrp = [openNavBtn, closeNavBtn];
+const heroSection = document.querySelector(".hero-section");
+const heroImg = document.querySelector(".hero__img");
+const mediaQuery = window.matchMedia("(max-width: 599px)");
 
 const toggleNavPanel = function () {
   navContent.classList.toggle("nav-content--hidden");
   navBgPanel.classList.toggle("background-panel--visible");
+};
+
+const handleViewportChange = function (e) {
+  if (e.matches) {
+    heroSection.classList.remove("container");
+    heroImg.src = "images/image-hero-mobile.png";
+  } else {
+    heroSection.classList.add("container");
+    heroImg.src = "images/image-hero-desktop.png";
+  }
 };
 
 expandableNavGroup.forEach((list) => {
@@ -21,3 +34,5 @@ navBtnGrp.forEach((btn) => {
 });
 
 navBgPanel.addEventListener("click", toggleNavPanel);
+
+mediaQuery.addEventListener("change", handleViewportChange);
